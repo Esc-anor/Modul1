023978743154b414cd7d05499a25507747dd586b
Роман <romangribanov@rambler.ru>
2024-07-25 22:02:59 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+home_work = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = (number_of_hours_spent / home_work)
+print('Курс:', course_name, ', всего задач: ', home_work , ', затрачено часов:', number_of_hours_spent, ', среднее время выполнения:', time_for_one_task)
