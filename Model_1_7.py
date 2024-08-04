48011f58c80c1b6da99ae47580cd130551a22bcf
Esc-anor <romangribanov@rambler.ru>
2024-08-04 03:10:20 +0300

    Можно проверять

--- Model_1_7.py
+++ Model_1_7.py
@@ -1,6 +1,5 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-print(sum(grades[0]) / (len(grades[0]))), (sum(grades[1]) / (len(grades[1])))
-print(sum(grades[2]) / (len(grades[2])))
-print(sum(grades[3]) / (len(grades[3])))
-print(sum(grades[4]) / (len(grades[4])))
+grades = [(sum(grades[0]) / (len(grades[0]))), (sum(grades[1]) / (len(grades[1]))), (sum(grades[2]) / (len(grades[2]))), (sum(grades[3]) / (len(grades[3]))), (sum(grades[4]) / (len(grades[4])))]
+students = set(sorted(students))
+print(dict(zip(sorted(students), grades)))
--- trener.py
+++ trener.py
@@ -1,3 +1,13 @@
 #git config --global user.name "Esc-anor"
 #git config --global user.email "romangribanov@rambler.ru"
 #git bash
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+print('g-', type(grades))
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+print('s-', type(students))
+grades = [(sum(grades[0]) / (len(grades[0]))), (sum(grades[1]) / (len(grades[1]))), (sum(grades[2]) / (len(grades[2]))), (sum(grades[3]) / (len(grades[3]))), (sum(grades[4]) / (len(grades[4])))]
+print(grades)
+a = students
+grades.sort()
+print(grades)
+print(type(students))
