bed0a76c2fa0e4197bb178e9bb6f052dbb7804f7
Esc-anor <romangribanov@rambler.ru>
2024-08-06 01:00:34 +0300

    Можно проверять

--- homework5.py
+++ homework5.py
@@ -1,8 +1,8 @@
 immutable_var = 1, 'String', True
 print(immutable_var)
 print(type(immutable_var))
-#immutable_var [2] = False # immutable_var - Кортеж так же, как и список, является коллекцией, но в отличие от него он неизменяем.(<class 'tuple'>)
-#чтобы immutable_var стал списком необходимо все значения взять в квадратные скобки и тогда ему присвоится <class 'list'>
+# immutable_var [2] = False # immutable_var - Кортеж так же, как и список, является коллекцией, но в отличие от него он неизменяем.(<class 'tuple'>)
+# чтобы immutable_var стал списком необходимо все значения взять в квадратные скобки и тогда ему присвоится <class 'list'>
 mutable_list = [5, 'apple', False]
 print(type(mutable_list))
 mutable_list[0] = 6
